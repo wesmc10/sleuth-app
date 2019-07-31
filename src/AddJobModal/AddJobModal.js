1707d4f95ba1daa0a033a198b7c217c544a21908
Wes McElroy <mcelroywes@gmail.com>
2019-07-30 18:25:40 -0700

    Began adding functionality to AddJobModal and hooking it up to API

--- src/AddJobModal/AddJobModal.js
+++ src/AddJobModal/AddJobModal.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './AddJobModal.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import config from '../config';
+import SleuthContext from '../SleuthContext';
 
 export default class AddJobModal extends Component {
     state = {
@@ -14,6 +18,8 @@ export default class AddJobModal extends Component {
         error: null
     };
 
+    static contextType = SleuthContext;
+
     componentDidMount() {
         document.addEventListener('mousedown', this.handleClick);
     }
@@ -26,6 +32,60 @@ export default class AddJobModal extends Component {
         if (!this.node.contains(e.target)) {
             this.props.closeModal();
         }
+    }
+
+    handleFormSubmission = (e) => {
+        e.preventDefault();
+        this.setState({
+            error: null
+        });
+        const { 
+            company,
+            position, 
+            location, 
+            salary, 
+            dateApplied, 
+            interviewDate,
+            applicationStatus, 
+            notes 
+        } = this.state;
+        const newJob = {
+            company,
+            position,
+            location,
+            salary,
+            dateApplied,
+            interviewDate,
+            applicationStatus,
+            notes,
+            user_id: this.context.currentUser.id
+        };
+
+        fetch(`${config.API_ENDPOINT}/api/jobs`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newJob)
+        })
+        .then(res =>
+            !res.ok
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()    
+        )
+        .then(res => {
+            this.setState({
+                company: '',
+                position: '',
+                location: '',
+                salary: '',
+                dateApplied: '',
+                interviewDate: '',
+                applicationStatus: '',
+                notes: ''
+            });
+            // continue adding here
+        })
     }
 
     handleCompanyChange = (e) => {
@@ -84,10 +144,16 @@ export default class AddJobModal extends Component {
         return (
             <div className={showOrHideModal}>
                 <section ref={node => this.node = node} className="AddJobModal_section">
-                    <form className="AddJobModal_form">
+                    <form className="AddJobModal_form" onSubmit={this.handleFormSubmission}>
                         <fieldset>
                             <section className="AddJobModal_add_job">
-                                <legend>Add a Job</legend>
+                                <legend className="AddJobModal_title">Add a Job</legend>
+                                <button
+                                    type="button"
+                                    className="close_modal_button"
+                                    onClick={this.props.closeModal}>
+                                        <FontAwesomeIcon icon={faTimes} />
+                                </button>
                                 <div role="alert">
                                     {error && <p className="red">{error}</p>}
                                 </div>
@@ -96,7 +162,6 @@ export default class AddJobModal extends Component {
                                     type="text"
                                     id="company"
                                     name="company"
-                                    placeholder="Company"
                                     required
                                     onChange={this.handleCompanyChange}
                                 />
@@ -105,7 +170,6 @@ export default class AddJobModal extends Component {
                                     type="text"
                                     id="position"
                                     name="position"
-                                    placeholder="Position"
                                     required
                                     onChange={this.handlePositionChange}
                                 />
@@ -114,7 +178,6 @@ export default class AddJobModal extends Component {
                                     type="text"
                                     id="location"
                                     name="location"
-                                    placeholder="Location"
                                     required
                                     onChange={this.handleLocationChange}
                                 />
@@ -123,7 +186,6 @@ export default class AddJobModal extends Component {
                                     type="text"
                                     id="salary"
                                     name="salary"
-                                    placeholder="Salary"
                                     onChange={this.handleSalaryChange}
                                 />
                                 <label htmlFor="date_applied">Date Applied</label>
@@ -131,7 +193,6 @@ export default class AddJobModal extends Component {
                                     type="date"
                                     id="date_applied"
                                     name="date_applied"
-                                    placeholder="Date Applied"
                                     required
                                     onChange={this.handleDateAppliedChange}
                                 />
@@ -140,26 +201,28 @@ export default class AddJobModal extends Component {
                                     type="date"
                                     id="interview_date"
                                     name="interview_date"
-                                    placeholder="Interview Date"
                                     onChange={this.handleInterviewDateChange}
                                 />
                                 <label htmlFor="application_status">Application Status</label>
-                                <input
-                                    type="text"
-                                    id="application_status"
-                                    name="application_status"
-                                    placeholder="Application Status"
-                                    required
-                                    onChange={this.handleApplicationStatusChange}
-                                />
+                                <select id="application_status">
+                                    <option value="applied">Applied</option>
+                                    <option value="phone">Phone</option>
+                                    <option value="technical">Technical</option>
+                                    <option value="on-site">On-site</option>
+                                    <option value="offer">Offer</option>
+                                    <option value="rejected">Rejected</option>
+                                </select>
                                 <label htmlFor="notes">Notes</label>
-                                <input
+                                <textarea
                                     type="text"
                                     id="notes"
                                     name="notes"
-                                    placeholder="Notes"
+                                    cols="30"
+                                    rows="5"
                                     onChange={this.handleNotesChange}
                                 />
+
+                                <button type="submit" className="AddJobModal_submit_button">Add Job</button>
                             </section>
                         </fieldset>
                     </form>
