e669b37adedb152f5d54ec6fb150843923ab230b
Wes McElroy <mcelroywes@gmail.com>
2019-08-02 21:18:05 -0700

    Began styling LandingPage component

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
 import TokenService from '../token-service';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 
 export default class LandingPage extends Component {
     componentDidMount() {
@@ -22,22 +24,26 @@ export default class LandingPage extends Component {
         return (
             <div className="LandingPage_main">
                 <div className="LandingPage_flex_container">
-                    <h1 className="LandingPage_title">Sleuth</h1>
-                    <h2 className="LandingPage_subtitle">A Tracker App for the Job Hunt</h2>
-                    <h3 className="LandingPage_tagline">Your job search starts &#x2014; and ends &#x2014; here</h3>
-    
-                    <button 
-                        type="button" 
-                        className="LandingPage_login_button"
-                        onClick={this.handleClickLogin}>
-                            Login
-                    </button>
-                    <button 
-                        type="button" 
-                        className="LandingPage_signup_button"
-                        onClick={this.handleClickSignUp}>
-                            Sign Up
-                    </button>
+                    <section className="LandingPage_content_section">
+                        <FontAwesomeIcon  icon={faUserSecret} className="LandingPage_icon"/>
+                        <h1 className="LandingPage_title">Sleuth</h1>
+                        <h2 className="LandingPage_subtitle">A Tracker App for the Job Hunt</h2>
+                        <h3 className="LandingPage_tagline">Your job search starts &#x2014; and ends &#x2014; here</h3>
+                    </section>
+                    <section className="LandingPage_buttons">
+                        <button 
+                            type="button" 
+                            className="LandingPage_login_button"
+                            onClick={this.handleClickLogin}>
+                                Login
+                        </button>
+                        <button 
+                            type="button" 
+                            className="LandingPage_signup_button"
+                            onClick={this.handleClickSignUp}>
+                                Sign Up
+                        </button>
+                    </section>
                 </div>
             </div>
         );
