246bb989691cc33b7411b62252315dc099437ff7
Wes McElroy <mcelroywes@gmail.com>
2019-08-03 11:30:04 -0700

    Added Favicon

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App/App';
+import Favicon from 'react-favicon';
 
 ReactDOM.render(
     <BrowserRouter>
+        <Favicon url="https://gitlab.computing.dcu.ie/uploads/-/system/project/avatar/904/fa-user-secret-200.png" />
         <App />
     </BrowserRouter>,
 document.getElementById('root'));
